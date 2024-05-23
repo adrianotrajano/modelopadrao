00773ab2eefb5906aa6a721125255ebde9da09e2
Adriano Trajano <adrianotjr@gmail.com>
2024-05-23 09:55:20 -0300

    commit inicial

---
+++ resources/views/clientes/show.blade.php
@@ -0,0 +1,37 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Detalhes do Cliente') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <p class="mb-4">Olá <strong>{{ Auth::user()->name }}</strong></p>
+
+                <p class="mb-4">
+                    Exibindo detalhes do Cliente <strong>{{ $cliente->nome }}</strong>
+                </p>
+
+                <p>
+                    <a href="{{ route('meus.clientes', Auth::user()->id) }}" class="bg-blue-500 text-white rounded p-2">Voltar</a>
+                    <a href="{{ route('cliente.edit', $cliente->id ) }}" class="bg-blue-500 text-white rounded p-2">Editar</a>
+                    <a href="{{ route('confirma.delete', $cliente->id) }}" class="bg-red-500 text-white rounded p-2">Excluir</a>
+                </p>
+
+                </div>
+                <div class="p-6 text-gray-900">
+                <p><strong>Nome: </strong>{{ $cliente->nome }}</p>
+                <p><strong>E-mail: </strong>{{ $cliente->email }} | <strong>Telefone: </strong>{{ $cliente->telefone }}</p>
+                <p><strong>Empresa: </strong>{{ $cliente->empresa }}</p>
+                <p><strong>Tel. Comercial: </strong>{{ $cliente->tel_comercial }}</p>
+
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
